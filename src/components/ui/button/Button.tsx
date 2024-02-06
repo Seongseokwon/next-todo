9c4c37d8405de94c91c1cbe0b8fffa7de222ead3
SeongSeokwon <pam9411@naver.com>
2024-02-06 10:18:18 +0900

    button 컴포넌트 추가

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,32 @@
+import styles from './Button.module.scss';
+import {ReactNode} from "react";
+
+interface ButtonStyleProps {
+    size?: 'xsm' | 'sm' | 'md' | 'lg' | 'xlg';
+    color?: 'default' | 'primary' | 'warning' | 'error'
+    borderStyle?: 'none'
+}
+
+interface ButtonProps extends ButtonStyleProps {
+    children: ReactNode;
+    type?: 'button' | 'submit' | 'reset';
+    onClick: () => void;
+}
+
+export default function Button(
+    {
+        children,
+        size = 'md',
+        type = 'button',
+        color = 'default',
+        borderStyle,
+        onClick}: ButtonProps) {
+    const buttonClasses = `
+    ${styles['btn']} 
+    ${styles[size]}
+    ${styles[color]} 
+    ${borderStyle ? styles[borderStyle] : ''}`
+    return <button type={type} className={buttonClasses} onClick={onClick}>
+        {children}
+    </button>
+}
